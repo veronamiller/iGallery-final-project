24689706708b79220cd21e87a8d6aac6dfbc6160
Verona <verona.m@outlook.de>
2021-11-03 21:51:17 +0100

    added OrderScreen.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import { applyMiddleware } from 'redux';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer} from './reducers/cartReducers'
 import { userLoginReducer , userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from './reducers/userReducers.js';
-import { orderCreateReducer } from './reducers/orderReducers.js';
+import { orderCreateReducer, orderDetailsReducer} from './reducers/orderReducers.js';
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -16,6 +16,7 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse
